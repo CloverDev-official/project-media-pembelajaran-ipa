947e35d0d6e1f01995ac1998c23e1bb4f7f87493
Ghaizan17 <ghaizan.pratama.maulana@gmail.com>
2025-08-18 13:35:33 +0800

    materi page
    ghaizan

--- shared/link.php
+++ shared/link.php
@@ -1,4 +1,5 @@
 <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+<script src="https://cdn.jsdelivr.net/npm/chart.js@4.4.4/dist/chart.umd.min.js"></script>
 <link rel="stylesheet" href="../style.css">
 <link rel="stylesheet" href="../src/output.css">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
